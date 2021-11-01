38005e33d4ae56f33c3eacd0950ad5479867cc8d
Eugene Dombrovski <eugene.dombrovski@helmes.com>
2021-11-01 17:44:26 +0300

    [ASSIST-3546] fix ItemGridThumbnail

--- src/elements/content-explorer/ItemGridThumbnail.js
+++ src/elements/content-explorer/ItemGridThumbnail.js
@@ -1,6 +1,5 @@
 // @flow
 import * as React from 'react';
-import isThumbnailReady from './utils';
 import { getIcon } from '../common/item/iconCellRenderer';
 import type { BoxItem } from '../../common/types/core';
 import './ItemGridThumbnail.scss';
@@ -10,20 +9,17 @@ type Props = {
 };
 
 const ItemGridThumbnail = ({ item }: Props) => {
-    let { thumbnailUrl } = item;
-    if (item.metadata) {
-        thumbnailUrl = item.metadata.enterprise_261189234.customThumbnail.url;
-    }
+    const customThumbnailUrl = item.metadata?.enterprise_261189234?.customThumbnail?.url;
 
     return (
         <div className="bce-ItemGridThumbnail">
-            {thumbnailUrl && isThumbnailReady(item) ? (
+            {customThumbnailUrl ? (
                 <div
                     className="bce-ItemGridThumbnail-item"
                     style={{
                         width: '100%',
                         height: '100%',
-                        background: `url("${thumbnailUrl}") center center / cover no-repeat`,
+                        background: `url("${customThumbnailUrl}") center center / cover no-repeat`,
                     }}
                 />
             ) : (
--- src/icons/folder/IconFolderCustom.tsx
+++ src/icons/folder/IconFolderCustom.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { Icon } from '../iconTypes';
 
 const IconFolderCustom = ({ className = '', height = 32, title, width = 32, thumbnailUrl }: Icon) => (
-    <img alt="Folder icon" src={thumbnailUrl} style={{ height: '100%', width: '100%', objectFit: 'cover' }} />
+    <img alt="Folder icon" src={thumbnailUrl} style={{ height, width }} />
 );
 
 export default IconFolderCustom;
